9f627ec2bccdb7296450a1112d4132cb0761f3c3
p0o <pooria_az@ymail.com>
2017-08-17 15:05:00 +0800

    refactor for better code re-use

--- src/loader.js
+++ src/loader.js
@@ -1,5 +1,28 @@
 import steem from 'steem';
 import SteemBotCore from './core';
+import { ALL_USERS } from './constants';
+
+function loadConfig(args) {
+  let targets;
+  let handler;
+
+  if (args.length > 2) {
+    throw(new Error('Your event function should only have one or two arguments'));
+  }
+
+  if (typeof(args[0]) === 'function') { // user omitted targets param
+    targets = ALL_USERS;
+    handler = args[0];
+  } else {
+    targets = typeof(args[0]) === 'string' ? [args[0]] : args[0];
+    handler = args[1];
+  }
+
+  return {
+    handler,
+    targets,
+  };
+}
 
 export default class SteemBot {
   constructor(username, activeKey) {
@@ -12,37 +35,16 @@ export default class SteemBot {
     }
   }
 
-  onDeposit(handler, targets = [this.username]) {
-    if (typeof(targets) === 'string') {
-      targets = [targets];
-    }
-
-    this.config.deposit = {
-      handler,
-      targets,
-    };
+  onDeposit(...args) {
+    this.config.deposit = getConfig(args);
   }
 
-  onPost(handler, targets = [this.username]) {
-    if (typeof(targets) === 'string') {
-      targets = [targets];
-    }
-
-    this.config.post = {
-      handler,
-      targets,
-    };
+  onPost(...args) {
+    this.config.post = getConfig(args);
   }
 
-  onComment(handler, targets = [this.username]) {
-    if (typeof(targets) === 'string') {
-      targets = [targets];
-    }
-
-    this.config.comment = {
-      handler,
-      targets,
-    };
+  onComment(...args) {
+    this.config.comment = getConfig(args);
   }
 
   start() {
