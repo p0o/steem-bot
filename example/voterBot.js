01c363ac23f77f3e990e8fb28134d9d66e416e70
p0o <pooria_az@ymail.com>
2017-08-24 16:09:11 +0800

    add example for voter bot

---
+++ example/voterBot.js
@@ -0,0 +1,29 @@
+// No need for the line below if your environment already supports ES6 with JavaScript import/export
+require('babel-register');
+
+// change the line below to:
+// const SteemBot = require('steem-bot').default
+// or:
+// import SteemBot from 'steem-bot';
+const SteemBot = require('../src/loader').default;
+
+
+const username = 'Your Steem Username';
+const postingKey = 'Your Posting Key Here!'; // Use environment variables instead of hardcoding to be safer
+
+const bot = new SteemBot({username, postingKey});
+
+bot.onComment('p0o-dev', handleComment);
+
+function handleComment(data, responder) {
+  console.log('user %s commented!', data.author);
+  console.log(data.body);
+
+  responder.comment('Hi there! I just upvoted you using SteemBot JavaScript library!');
+  responder.upvote(); // 100% upvote
+  /*
+  responder.downvote(5); // 5% downvote (flag)
+  */
+}
+
+bot.start();
